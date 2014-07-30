01f38460be3ba2a6ae38b7a4f762f9977de1fad3
Simon Larsen <simonhffh@gmail.com>
2014-07-30 04:28:29 +0200

    Game: Added basic conf.lua

---
+++ src/game/conf.lua
@@ -0,0 +1,19 @@
+function love.conf(t)
+    t.identity = "proximitycore"
+    t.version = "0.9.1"
+    t.console = false
+
+    t.window.title = "Proximity Core"
+    t.window.icon = nil
+    t.window.width = 800
+    t.window.height = 600
+    t.window.borderless = false
+    t.window.resizable = false
+    t.window.fullscreen = false
+    t.window.fullscreentype = "normal"
+    t.window.vsync = true
+    t.window.fsaa = 0
+    t.window.display = 1
+    t.window.highdpi = false
+    t.window.srgb = false
+end
